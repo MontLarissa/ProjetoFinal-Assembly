d4acad7de148399867d2fcce972a5fd0946f15d9
Sichiroli <sichiroli07@gmail.com>
2024-11-15 21:50:33 -0300

    Inserecord

--- ImprimeInterfaceTeste.asm
+++ ImprimeInterfaceTeste.asm
@@ -85,175 +85,200 @@ LETRAS MACRO
 ENDM
 ;!!Ajeitar as matrizes!!;
 .DATA
-    MATRIZMAPA   DW 0,0,0,0,0,0,1,1,1,0     ,     1,1,1,0,0,0,0,0,0,0
-                 DW 0,0,1,0,0,0,0,1,0,0     ,     0,0,0,0,1,0,0,0,0,0
-                 DW 0,0,1,0,0,0,0,0,0,0     ,     0,0,0,1,1,0,0,0,0,0
-                 DW 0,0,0,0,0,0,0,1,0,0     ,     0,0,0,0,1,0,1,1,0,0
-                 DW 0,0,0,0,0,0,0,1,1,0     ,     0,0,0,0,0,0,0,0,0,0
-                 DW 0,0,0,0,0,0,0,1,0,0     ,     1,1,1,1,0,0,0,0,0,0
-                 DW 1,1,1,0,0,0,0,0,0,0     ,     0,0,0,0,0,0,0,1,0,0
-                 DW 0,0,0,0,0,1,1,0,0,0     ,     1,1,0,0,0,0,0,1,1,0
-                 DW 0,0,0,0,0,0,0,0,0,0     ,     0,0,0,0,0,0,0,1,0,0
-                 DW 1,1,1,1,0,0,0,0,0,0     ,     0,0,0,0,0,0,0,0,0,0
+    MATRIZMAPA         DW 0,0,0,0,0,0,1,1,1,0     ,     1,1,1,0,0,0,0,0,0,0
+                       DW 0,0,1,0,0,0,0,1,0,0     ,     0,0,0,0,1,0,0,0,0,0
+                       DW 0,0,1,0,0,0,0,0,0,0     ,     0,0,0,1,1,0,0,0,0,0
+                       DW 0,0,0,0,0,0,0,1,0,0     ,     0,0,0,0,1,0,1,1,0,0
+                       DW 0,0,0,0,0,0,0,1,1,0     ,     0,0,0,0,0,0,0,0,0,0
+                       DW 0,0,0,0,0,0,0,1,0,0     ,     1,1,1,1,0,0,0,0,0,0
+                       DW 1,1,1,0,0,0,0,0,0,0     ,     0,0,0,0,0,0,0,1,0,0
+                       DW 0,0,0,0,0,1,1,0,0,0     ,     1,1,0,0,0,0,0,1,1,0
+                       DW 0,0,0,0,0,0,0,0,0,0     ,     0,0,0,0,0,0,0,1,0,0
+                       DW 1,1,1,1,0,0,0,0,0,0     ,     0,0,0,0,0,0,0,0,0,0
 
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
-                 DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
+                       DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
                      
 MATRIZIMPRESSÃO DW 10 DUP(10 DUP('~'))
-    LETRA        DW 41H,42H,43H,44H,45H,46H,47H,48H,49H,04AH
-
-    VETOR        DB 10 DUP (0)
+    LETRA              DW 41H,42H,43H,44H,45H,46H,47H,48H,49H,04AH
+    ARMAZENACORDENADA  DW 2 DUP (0)
+    ACERTOCONTADOR     DB 19
+    TENTATIVAS         DB 80
     ;Mensagens;
     ;Pagina Inicial;
-    LOGO1        DB 13,10,'              ===================================================              ', '$'
-    LOGO2        DB 13,10,'              =                                                 =            ', '$'
-    LOGO3        DB 13,10,'              =     Batalha     Naval     Em     Assembly       =            ','$'
-    LOGO4        DB 13,10,'              =                                                 =            ','$'
-    LOGO5        DB 13,10,'              ===================================================              ', 13,10, '$'
-    ENTMSG1      DB 13,10,'              Insira ate 3 numeros de 0-9 para iniciar o jogo:', '$'
-    MSGCONTROLE  DB 13,10,'OK1', '$'
-    MSGCONTROLE1 DB 13,10,'OK2', '$'
-    MSGCONTROLE2 DB 13,10,'OK3', '$'
-    MSGCONTROLE3 DB 13,10,'OK4', '$'
+    LOGO1              DB 13,10,'              ===================================================              ', '$'
+    LOGO2              DB 13,10,'              =                                                 =            ', '$'
+    LOGO3              DB 13,10,'              =     Batalha     Naval     Em     Assembly       =            ','$'
+    LOGO4              DB 13,10,'              =                                                 =            ','$'
+    LOGO5              DB 13,10,'              ===================================================              ', 13,10, '$'
+    ENTMSG1            DB 13,10,'              Insira ate 3 numeros de 0-9 para iniciar o jogo:', '$'
+    MSGCONTROLE        DB 13,10,'OK1', '$'
+    MSGCONTROLE1       DB 13,10,'OK2', '$'
+    MSGCONTROLE2       DB 13,10,'OK3', '$'
+    MSGCONTROLE3       DB 13,10,'OK4', '$'
+
     PEDECORDENADA      DB 13,10,'              Informe a cordenada do tiro:', '$'
     CORDENADANAOACEITA DB 13,10,'              Cordenada nao encontrada, tente novamente', '$'
     ACERTODETIRO       DB 13,10,'              Acerto de tiro!', '$'
     ERRODETIRO         DB 13,10,'              Nao ha inimigos nessa cordenada.', '$'
     SAIRJOGO           DB 13,10,'              Para sair do jogo aperte ESC', '$'
+    DESISTIUMSGDB      DB 13,10,'              Voce desistiu do jogo, que pena', '$'
 
 
 .CODE
 MAIN PROC
     ;Acesso ao DATA
-                     MOV               AX, @DATA
-                     MOV               DS,AX
+                         MOV               AX, @DATA
+                         MOV               DS,AX
 
-                     CALL              INICIAR
-                     CALL              IMPRIMEINTERFACE
+                         CALL              INICIAR
+                         CALL              INTERFACE
     ;Termina o programa
-                     MOV               AH,4CH
-                     INT               21H
+                         MOV               AH,4CH
+                         INT               21H
 MAIN ENDP
 INICIAR PROC
-                     LIMPA_TELA
-                     IMPMENSAG         LOGO1
-                     IMPMENSAG         LOGO2
-                     IMPMENSAG         LOGO3
-                     IMPMENSAG         LOGO4
-                     IMPMENSAG         LOGO5
-                     IMPMENSAG         ENTMSG1
-                     MOV               CX, 3
-                     XOR               BX,BX
-                     XOR               DX,DX
-                     MOV               AH,1
-    LerEnt:          
-                     INT               21h
-                     CMP               AL, 0DH
-                     JE                CompENT
-                     MOV               DL,AL
-                     ADD               BL, DL
-                     LOOP              LerEnt
-    CompENT:         
-                     CMP               BL, 4
-                     JB                PulaParaFim
+                         LIMPA_TELA
+                         IMPMENSAG         LOGO1
+                         IMPMENSAG         LOGO2
+                         IMPMENSAG         LOGO3
+                         IMPMENSAG         LOGO4
+                         IMPMENSAG         LOGO5
+                         IMPMENSAG         ENTMSG1
+                         MOV               CX, 3
+                         XOR               BX,BX
+                         XOR               DX,DX
+                         MOV               AH,1
+    LerEnt:              
+                         INT               21h
+                         CMP               AL, 0DH
+                         JE                CompENT
+                         MOV               DL,AL
+                         ADD               BL, DL
+                         LOOP              LerEnt
+    CompENT:             
+                         CMP               BL, 4
+                         JB                PulaParaFim
 
-    DivDerminadaM:   
-                     MOV               AX, BX
-                     MOV               BL, 4
-                     DIV               BL
-    CompQUA:         
-                     CMP               AH, 1
-                     JE                QUA1
+    DivDerminadaM:       
+                         MOV               AX, BX
+                         MOV               BL, 4
+                         DIV               BL
+    CompQUA:             
+                         CMP               AH, 1
+                         JE                QUA1
 
-                     CMP               AH, 2
-                     JE                QUA2
+                         CMP               AH, 2
+                         JE                QUA2
 
-                     CMP               AH, 3
-                     JE                QUA3
+                         CMP               AH, 3
+                         JE                QUA3
                       
-                     CMP               AH, 0
-                     JE                QUA4
-    PulaParaFim:     
-                     JMP               RetornaEnt
-    QUA1:            
-                     Controle_Programa MSGCONTROLE
-                     INFORMATRIZ       0,0,360
-                     Controle_Programa MSGCONTROLE
-                     SALVAMJOGO
-                     JMP               RetornaEnt
-    QUA2:            
+                         CMP               AH, 0
+                         JE                QUA4
+    PulaParaFim:         
+                         JMP               RetornaEnt
+    QUA1:                
+                         Controle_Programa MSGCONTROLE
+                         INFORMATRIZ       0,0,360
+                         Controle_Programa MSGCONTROLE
+                         SALVAMJOGO
+                         JMP               RetornaEnt
+    QUA2:                
 
-                     Controle_Programa MSGCONTROLE1
-                     INFORMATRIZ       20,0,360
-                     Controle_Programa MSGCONTROLE1
-                     SALVAMJOGO
-                     JMP               RetornaEnt
-    QUA3:            
+                         Controle_Programa MSGCONTROLE1
+                         INFORMATRIZ       20,0,360
+                         Controle_Programa MSGCONTROLE1
+                         SALVAMJOGO
+                         JMP               RetornaEnt
+    QUA3:                
 
-                     Controle_Programa MSGCONTROLE2
-                     INFORMATRIZ       0,400,760
-                     Controle_Programa MSGCONTROLE2
-                     SALVAMJOGO
-                     JMP               RetornaEnt
-    QUA4:            
-                     Controle_Programa MSGCONTROLE3
-                     INFORMATRIZ       20,400,760
-                     Controle_Programa MSGCONTROLE3
-                     SALVAMJOGO
-                     JMP               RetornaEnt
-    RetornaEnt:      
-                     VOLTAVALOR
-                     RET
+                         Controle_Programa MSGCONTROLE2
+                         INFORMATRIZ       0,400,760
+                         Controle_Programa MSGCONTROLE2
+                         SALVAMJOGO
+                         JMP               RetornaEnt
+    QUA4:                
+                         Controle_Programa MSGCONTROLE3
+                         INFORMATRIZ       20,400,760
+                         Controle_Programa MSGCONTROLE3
+                         SALVAMJOGO
+                         JMP               RetornaEnt
+    RetornaEnt:          
+                         VOLTAVALOR
+                         RET
 INICIAR ENDP
 INTERFACE PROC
-                   LIMPA_TELA
-                   SALVAMJOGO
-                   JMP               INICIO
-    ERRORCOR:      
-                   IMPMENSAG         CORDENADANAOACEITA
-    INICIO:        
-                   IMPMENSAG         PEDECORDENADA
+                         PULA_LINHA
+                         SALVAMJOGO
     
-                   LEA               SI, ARMAZENACORDENADA
-                   MOV               CX, 2
-                   MOV               AH, 01
-    PEGACORDENADAS:
-                   CMP AL,27H
-                   MOV               DX,AX
-                   INT               21h
-                   MOV               [SI],DX
-                   ADD               SI,2
-                   LOOP              PEGACORDENADAS
+    INICIO:              
+                         IMPMENSAG         PEDECORDENADA
+    
+                         XOR               SI, SI
+                         MOV               CX, 2
+                         MOV               AH, 1
+    Ler:                 
+                         INT               21h
+                         CMP               AL, 01BH
+                         JE                ESCACIONADO
+                         MOV               DL, AL
+                         MOV               ARMAZENACORDENADA[SI], DX
+                         ADD               SI, 2
+                         LOOP              Ler
 
-    CONVERTELETRA: 
-                   MOV SI,0
-                   MOV               AX,[SI]
-                   JMP               COMPL
-    MINUSCULO:     
-                   SUB               AX, 20H
-    COMPL:         
-                   CMP               AX, 4AH
-                   JGE MINUSCULO
-                   SUB               AX,41H
-                   MOV BX, 2
-                   MUL BX
-                   MOV [SI],DX
-CONVERTENUMERO: 
-                   MOV SI,2
-                   MOV               AX,[SI]
-    COMPN:         
-                   SUB               AX,30H
-                   MOV BX, 2
-                   MUL BX
-                   MOV [SI],DX
-                   VOLTAVALOR
-                   RET                  
+                         CALL              CONVERSAODECORDENADA
+
+                         XOR               SI,SI
+VERIFICAÇÃO:
+                         MOV               AX,ARMAZENACORDENADA[SI]
+                         CMP               AX,18H
+                         JBE               SAIR
+    ERRORCOR:            
+                         IMPMENSAG         CORDENADANAOACEITA
+                         JMP               INICIO
+    ESCACIONADO:         
+                CALL SAIRESC
+    SAIR:                
+                         VOLTAVALOR
+                         RET
 INTERFACE ENDP
+CONVERSAODECORDENADA PROC
+                         LEA               SI, ARMAZENACORDENADA
+                         MOV               AX,[SI]
+                         JMP               COMPL
+    MINUSCULO:           
+                         SUB               AX, 20H
+    COMPL:               
+                         CMP               AX, 4AH
+                         JG                MINUSCULO
+                         SUB               AX,41H
+                         ADD               AX,AX
+                         MOV               [SI],AX
+
+    CONVERTENUMERO:      
+                         ADD               SI,2
+                         MOV               AX,[SI]
+    COMPN:               
+                         SUB               AX,30H
+                         ADD               AX,AX
+                         MOV               [SI],AX
+
+                         RET
+CONVERSAODECORDENADA ENDP
+SAIRESC PROC
+                         LIMPA_TELA
+                         IMPMENSAG         DESISTIUMSGDB
+                         MOV               AH,4CH
+                         INT               21H
+                         RET
+SAIRESC ENDP
+END MAIN
--- Versão Definitiva.asm
+++ Versão Definitiva.asm
@@ -103,6 +103,9 @@ ENDM
                      
     MATRIZIMPRESSÃO DW 10 DUP(10 DUP('~'))
     LETRA        DW 41H,42H,43H,44H,45H,46H,47H,48H,49H,04AH
+    ARMAZENACORDENADA  DW 2 DUP (0)
+    ACERTOCONTADOR     DB 19
+    TENTATIVAS         DB 80
     ;Mensagens;
     ;Pagina Inicial;
     LOGO1        DB 13,10,'              ===================================================              ', '$'
@@ -111,6 +114,13 @@ ENDM
     LOGO4        DB 13,10,'              =                                                 =            ','$'
     LOGO5        DB 13,10,'              ===================================================              ', 13,10, '$'
     ENTMSG1      DB 13,10,'              Insira ate 3 numeros de 0-9 para iniciar o jogo:', '$'
+
+    PEDECORDENADA      DB 13,10,'              Informe a cordenada do tiro:', '$'
+    CORDENADANAOACEITA DB 13,10,'              Cordenada nao encontrada, tente novamente', '$'
+    ACERTODETIRO       DB 13,10,'              Acerto de tiro!', '$'
+    ERRODETIRO         DB 13,10,'              Nao ha inimigos nessa cordenada.', '$'
+    SAIRJOGO           DB 13,10,'              Para sair do jogo aperte ESC', '$'
+    DESISTIUMSGDB      DB 13,10,'              Voce desistiu do jogo, que pena', '$'
 .CODE
 MAIN PROC
     ;Acesso ao DATA
@@ -119,6 +129,7 @@ MAIN PROC
 
                      CALL              INICIAR
                      CALL              IMPRIMEINTERFACE
+                     CALL              INTERFACE
     ;Termina o programa
                      MOV               AH,4CH
                      INT               21H
@@ -186,57 +197,69 @@ INICIAR PROC
                      VOLTAVALOR
                      RET
 INICIAR ENDP
-IMPRIMEINTERFACE PROC
-                     SALVAMJOGO
-                     LIMPA_TELA
-                     Pula_linha
-                     TAB
-                     TAB
-ESPAÇO
-                     MOV               CX, 10
-                     MOV               AL, 30H
-                     MOV               AH, 02H
-    NUMEROS1:        
-ESPAÇO
-ESPAÇO
-ESPAÇO
-                     NUMEROS
-                     LOOP              NUMEROS1
+INTERFACE PROC
+                         PULA_LINHA
+                         SALVAMJOGO
+    
+    INICIO:              
+                         IMPMENSAG         PEDECORDENADA
+    
+                         XOR               SI, SI
+                         MOV               CX, 2
+                         MOV               AH, 1
+    Ler:                 
+                         INT               21h
+                         CMP               AL, 01BH
+                         JE                ESCACIONADO
+                         MOV               DL, AL
+                         MOV               ARMAZENACORDENADA[SI], DX
+                         ADD               SI, 2
+                         LOOP              Ler
 
-    MATRIZELETRAS:   
-                     PULA_LINHA
-                     TAB
-                     TAB
-                     LEA               DI, LETRA
-                     XOR               BX, BX
-                     XOR               SI, SI
-                     MOV               CX, 10
-                     JMP               L1
-    MUDALINHA:       
-                     XOR               BX, BX
-                     ADD               SI, 20
-                     MOV               CX, 10
-                     CMP               SI, 180
-                     PULA_LINHA
-                     JG                FIM
-    L1:              
-                     PULA_LINHA
-                     TAB
-                     TAB
-                     MOV               AH, 02H
-                     LETRAS
-    IMPRIMELINHA:    
-ESPAÇO
-ESPAÇO
-ESPAÇO
-                     MOV               DX, MATRIZIMPRESSÃO[SI][BX]
-                     OR                DL, 30H
-                     INT               21H
-                     ADD               BX, 2
-                     LOOP              IMPRIMELINHA
-                     JMP               MUDALINHA
-    FIM:             
-                     VOLTAVALOR
-                     RET
-IMPRIMEINTERFACE ENDP
+                         CALL              CONVERSAODECORDENADA
+
+                         XOR               SI,SI
+VERIFICAÇÃO:
+                         MOV               AX,ARMAZENACORDENADA[SI]
+                         CMP               AX,18H
+                         JBE               SAIR
+    ERRORCOR:            
+                         IMPMENSAG         CORDENADANAOACEITA
+                         JMP               INICIO
+    ESCACIONADO:         
+                CALL SAIRESC
+    SAIR:                
+                         VOLTAVALOR
+                         RET
+INTERFACE ENDP
+CONVERSAODECORDENADA PROC
+                         LEA               SI, ARMAZENACORDENADA
+                         MOV               AX,[SI]
+                         JMP               COMPL
+    MINUSCULO:           
+                         SUB               AX, 20H
+    COMPL:               
+                         CMP               AX, 4AH
+                         JG                MINUSCULO
+                         SUB               AX,41H
+                         ADD               AX,AX
+                         MOV               [SI],AX
+
+    CONVERTENUMERO:      
+                         ADD               SI,2
+                         MOV               AX,[SI]
+    COMPN:               
+                         SUB               AX,30H
+                         ADD               AX,AX
+                         MOV               [SI],AX
+
+                         RET
+CONVERSAODECORDENADA ENDP
+SAIRESC PROC
+                         LIMPA_TELA
+                         IMPMENSAG         DESISTIUMSGDB
+                         MOV               AH,4CH
+                         INT               21H
+                         RET
+SAIRESC ENDP
 END MAIN
